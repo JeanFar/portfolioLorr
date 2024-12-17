30692b89a8f1f8bdba2c48ed69086317f70427a1
JeanFar <142344133+JeanFar@users.noreply.github.com>
2024-12-16 22:23:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function imprimirFormulario() {
+    window.print(); // Esta função abre a janela de impressão
+}
+
+
+// Selecione o elemento #about
+const aboutElement = document.querySelector('#about');
+
+// Crie o observador de interseção
+const observer = new IntersectionObserver((entries, observer) => {
+entries.forEach(entry => {
+    // Se o #about estiver na tela, adiciona a classe .in-view
+    if (entry.isIntersecting) {
+        entry.target.classList.add('in-view');
+    } else {
+        // Se o #about sair da tela, remove a classe .in-view
+        entry.target.classList.remove('in-view');
+    }
+});
+}, {
+threshold: 0.5 // Define que 50% do elemento precisa estar visível para ativar a classe
+});
+
+// Comece a observar o elemento #about
+observer.observe(aboutElement);
